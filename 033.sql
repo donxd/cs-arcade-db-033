385deb656c0af1f1eb4aaac73a1d1bf22f61b88c
donxd <n0s3.xd@gmail.com>
2019-10-02 14:01:04 -0500

    initial commit of arcade-db/033

---
+++ 033.sql
@@ -0,0 +1,5 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE orderingEmails()
+BEGIN
+    SELECT id, email_title, CASE WHEN size >= 1048576 THEN CONCAT(FLOOR((size / 1048576)),' Mb') ELSE CONCAT(FLOOR((size / 1024)),' Kb') END AS short_size FROM emails ORDER BY size DESC;
+END
---
+++ README.md
@@ -0,0 +1,1 @@
+arcade-db 033 - resolution
